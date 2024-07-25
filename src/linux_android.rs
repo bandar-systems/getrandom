b1f269e04ea549df61fbec7aa3d6d9009780368f
Stephen Balogh <stephen@s2.dev>
2024-07-24 17:19:08 -0700

    linux use libc getrandom

--- src/linux_android.rs
+++ src/linux_android.rs
@@ -8,12 +8,18 @@ pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
 
 // Also used by linux_android_with_fallback to check if the syscall is available.
 pub fn getrandom_syscall(buf: &mut [MaybeUninit<u8>]) -> libc::ssize_t {
-    unsafe {
-        libc::syscall(
-            libc::SYS_getrandom,
-            buf.as_mut_ptr().cast::<core::ffi::c_void>(),
-            buf.len(),
-            0,
-        ) as libc::ssize_t
+    if cfg!(feature = "system-test") {
+        unsafe {
+            libc::getrandom(buf.as_mut_ptr() as *mut libc::c_void, buf.len(), 0) as libc::ssize_t
+        }
+    } else {
+        unsafe {
+            libc::syscall(
+                libc::SYS_getrandom,
+                buf.as_mut_ptr().cast::<core::ffi::c_void>(),
+                buf.len(),
+                0,
+            ) as libc::ssize_t
+        }
     }
 }
